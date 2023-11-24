bf8d9bfab39b64824a93d67252ba94c0746927e6
Ana <Ana@HP250NOTEBOOK>
2023-11-24 09:45:22 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Legian_Ana_lab7;
+
+public partial class AboutPage : ContentView
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Legian_Ana_lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
